999eb39da5b93c764887d02c80d8c1a44c5bc65f
Filip Gieracki <filip.gieracki@gmail.com>
2023-10-10 17:01:38 +0200

    Initial commit

---
+++ main.py
@@ -0,0 +1,11 @@
+def main():
+    wordCount = {}
+    link = "https://www.ic.unicamp.br/~stolfi/voynich/mirror/reeds/docs/FSG.txt"
+    # fetch the content from the url
+
+
+
+# Press the green button in the gutter to run the script.
+if __name__ == '__main__':
+    main()
+
